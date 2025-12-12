1c35a7c86b644e3997b46f8598eba39dddbb84de
Georges Byona <georgesbyona@gmail.com>
2025-12-12 21:57:46 +0300

    Adding .env file

--- services/firebase.ts
+++ services/firebase.ts
@@ -16,15 +16,15 @@ import {
 import { getStorage, ref, uploadString, getDownloadURL } from "firebase/storage";
 import { UserProfile, UserRole, UserStats } from "../types";
 
-// Configuration provided by user
+// Configuration loaded from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAPRvsPk37HKRMHwc7T6gg757vYpBUivKc",
-  authDomain: "vut-elimu.firebaseapp.com",
-  databaseURL: "https://vut-elimu-default-rtdb.firebaseio.com",
-  projectId: "vut-elimu",
-  storageBucket: "vut-elimu.firebasestorage.app",
-  messagingSenderId: "879266000392",
-  appId: "1:879266000392:web:612a615950868593682bbd"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase directly (Online Mode)
